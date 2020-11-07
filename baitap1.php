481d60322f3fff8a4b816fd9c7aed1b6a49353d5
Yumibk82 <ttthang0102@gmail.com>
2020-11-07 11:01:01 +0700

    bt

---
+++ baitap1.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <h1>Vi du ve cach nhung CSS</h1>
+    <style>
+        p{
+            color: red;
+        }
+    </style>
+    <link rel="stylesheet" type="text/css" href="bt1.css">
+</head>
+<body>
+<p style="color: blue">This is an ordinary paragraph</p>
+<p>Notice that this text is blue</p>
+<p>The default text color for a page is defined in the body selector</p>
+
+
+</body>
+</html>
