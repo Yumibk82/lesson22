3c006705b1b82b221b8b34641fb4e6169e8adc28
Yumibk82 <ttthang0102@gmail.com>
2020-11-08 04:20:57 +0700

    bt2

---
+++ baitap2.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <h1>Việt Nam</h1>
+    <hr>
+    <p style="font-size: small">Bách khoa toàn thư mở Wikipedia</p>
+</head>
+<body>
+<p> <b>Việt Nam </b> (tên chính thức: <b>Cộng hòa Xã hội chủ nghĩa Việt Nam </b>) là một quốc gia nằm ở phía đông, ở giữa Lào và Campuchia, thuộc <span style="color: deepskyblue"> bán đảo Đông Dương </span> , thuộc khu vực <span style="color: blue"> Đông Nam Á </span> thuộc châu Á. </p>
+<p> Phía bắc Việt Nam giáp với <span style="color: blue"> Trung Quốc </span>, phía tây Việt Nam giáp với <span style="color: blue"> Lào </span> và  <span style="color: blue"> Campuchia</span>  </p>
+</body>
+</html>
